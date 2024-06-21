72269805d3f1cf35cbf2ac458c9ea307bbc2b962
mucyochris47 <mucyochris250@gmail.com>
2024-06-21 12:26:51 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,19 +4,17 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    putchar('0');
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
-    putchar('\n');
-
-    return (0);
+         int c;
+	     
+	 c= 0;
+	    while
+		   (c< 10) {
+		          putchar(c + '0');
+	                  c++;
+	            }	     
+          putchar('\n');
+          return (0);
 }
