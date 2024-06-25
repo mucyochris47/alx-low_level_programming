f808926f6aa11d0743e5859ab5afc7779b3bb126
mucyochris47 <mucyochris250@gmail.com>
2024-06-25 13:19:59 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/* File: 10-add.c */
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer to add
+ * @b: second integer to add
+ *
+ * Return: sum of @a and @b
+ */
+int add(int a, int b)
+{
+    return a + b;
+}
