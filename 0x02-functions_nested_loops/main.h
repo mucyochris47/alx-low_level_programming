c9e849e92b972e7ab2cea64ebada23097cb84e21
mucyochris47 <mucyochris250@gmail.com>
2024-06-25 11:15:26 -0700

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
+
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -12,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
- #endif
+
+#endif
