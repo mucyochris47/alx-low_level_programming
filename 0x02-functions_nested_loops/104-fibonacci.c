d0d2b2b02204511c71813c7dc0036ed6da03545c
mucyochris47 <mucyochris250@gmail.com>
2024-06-26 08:53:28 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,7 +4,7 @@ int main() {
     int first = 1, second = 2, next;
     int count;
 
-    printf("%d, %d", first, second); // Print the first two Fibonacci numbers
+    printf("%d, %d", first, second); /* Print the first two Fibonacci numbers */
 
     for (count = 3; count <= 98; ++count) {
         next = first + second;
