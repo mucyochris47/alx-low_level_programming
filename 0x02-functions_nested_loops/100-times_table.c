d23264c59c0772bf3d2385b98d00cf4a17311e93
mucyochris47 <mucyochris250@gmail.com>
2024-06-26 08:27:17 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  * @n: The number of times table to print (0 <= n <= 15)
  */
 void print_times_table(int n) {
-    int i, j; // Declarations moved to the beginning of the function
+    int i, j; /* Declarations moved to the beginning of the function */
 
     /* Exit function if n is out of valid range */
     if (n < 0 || n > 15) {
@@ -17,11 +17,11 @@ void print_times_table(int n) {
         for (j = 0; j <= n; j++) {
             int result = i * j;
             if (j == 0) {
-                printf("%d", result); // First element of the row
+                printf("%d", result); /* First element of the row */
             } else {
-                printf(", %d", result); // Rest of the elements
+                printf(", %d", result); /* Rest of the elements */
             }
         }
-        printf("\n"); // Move to the next row
+        printf("\n"); /* Move to the next row */
     }
 }
