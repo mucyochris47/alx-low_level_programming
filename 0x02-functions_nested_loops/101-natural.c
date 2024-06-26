562fad885283fbf48c014213a21925f421691e21
mucyochris47 <mucyochris250@gmail.com>
2024-06-26 08:33:28 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int limit = 1024;
+    int sum = 0;
+    int i;
+
+    for (i = 1; i < limit; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
