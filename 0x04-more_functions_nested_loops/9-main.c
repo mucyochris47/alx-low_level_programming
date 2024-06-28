374c958ff5d062797215564256a682f38ff9ec06
mucyochris47 <mucyochris250@gmail.com>
2024-06-28 10:29:43 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void fizz_buzz(void); // Prototype of the Fizz-Buzz function defined in 9-fizz_buzz.c
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    fizz_buzz(); // Call the Fizz-Buzz function
+
+    return 0;
+}
