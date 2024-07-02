62368407fd43cc24658ebdfc6378403a1328d7a1
mucyochris47 <mucyochris250@gmail.com>
2024-07-02 13:09:16 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Updates the value pointed to by n to 98.
- * @n: Pointer to an integer variable to update
+ * reset_to_98 - updates the value of the integer it points to to 98.
+ * @n: pointer to an integer
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+    *n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <studio.h>
+#include <stdio.h>
 #include "main.h"
 
 void reset_to_98(int *n);
