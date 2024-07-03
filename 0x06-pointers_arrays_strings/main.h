72a5be25d29b46a3b1f2d20850f36a59424c1292
mucyochris47 <mucyochris250@gmail.com>
2024-07-03 10:10:52 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int);
+void print_triangle(int);
+void print_square(int size);
+
+#endif /* MAIN_H */
