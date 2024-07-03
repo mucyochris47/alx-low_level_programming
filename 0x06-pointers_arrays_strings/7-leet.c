daad1dd4a8a5b7ac27ae153b011ba2527e6dec89
mucyochris47 <mucyochris250@gmail.com>
2024-07-03 12:25:11 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
-int main(void)
+/**
+ * leet - Encodes a string into 1337 (leet) format.
+ * @str: The string to encode.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *leet(char *str)
 {
-    char str[] = "Hello, World!";
+    int i = 0;
+    char leet_map[10] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't', '\0', 'r'};
 
-    printf("Original: %s\n", str);
-    printf("Leetified: %s\n", leet(str));
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = leet_map[str[i] - 'a'];
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = leet_map[str[i] - 'A'];
+        }
+        i++;
+    }
 
-    return 0;
+    return str;
 }
