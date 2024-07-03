afe47b8e406a1bdb6e2e58120ab1a44e7e504aaf
mucyochris47 <mucyochris250@gmail.com>
2024-07-03 01:37:26 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,31 @@
-#include <unistd.h>
+/* 3-puts.c */
+
+#include "main.h"
 
 /**
  * _putchar - Writes a character to stdout.
  * @c: The character to print.
  *
- * Return: 1 on success, -1 on error.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
     return write(1, &c, 1);
 }
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: The string to print.
+ */
+void _puts(char *str)
+{
+    while (*str != '\0')
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n'); /* Print new line after the string */
+}
+
+/* Your other code if present */
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,9 +4,11 @@
  * _putchar - writes a character to stdout
  * @c: The character to print
  *
- * Return: 1 on success, -1 on error
+ * Return: 1 on success
+ * on error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
+
     return write(1, &c, 1);
 }
