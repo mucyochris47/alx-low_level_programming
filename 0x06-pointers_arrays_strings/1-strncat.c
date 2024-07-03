c18bd452d186ace48ab9ccd8a8c4d6232b903e22
mucyochris47 <mucyochris250@gmail.com>
2024-07-03 11:57:59 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,15 +13,15 @@ char *_strncat(char *dest, const char *src, int n)
     int dest_len = 0;
     int i = 0;
 
-    // Find the length of dest
+    /* Find the length of dest */
     while (dest[dest_len] != '\0')
         dest_len++;
 
-    // Concatenate src to dest
+    /* Concatenate src to dest */
     for (i = 0; i < n && src[i] != '\0'; i++)
         dest[dest_len + i] = src[i];
 
-    dest[dest_len + i] = '\0'; // Null-terminate the concatenated string
+    dest[dest_len + i] = '\0'; /* Null-terminate the concatenated string */
 
     return dest;
 }
