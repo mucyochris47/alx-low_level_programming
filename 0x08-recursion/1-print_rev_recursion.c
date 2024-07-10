50db8ffc40fb8398fdc0fe08141712b223bc5d5f
mucyochris47 <mucyochris250@gmail.com>
2024-07-10 11:01:34 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: String to be printed in reverse
+ *
+ * Description: Uses recursion to print each character of
+ * the string in reverse order until the null character is reached.
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+        return;
+
+    _print_rev_recursion(s + 1); /* Recursively move to end of string */
+    _putchar(*s); /* Print current character after recursion */
+}
