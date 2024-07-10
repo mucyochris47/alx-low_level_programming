ef453a7c032d4f0d06c7b2d348f764558276ee63
mucyochris47 <mucyochris250@gmail.com>
2024-07-10 11:41:24 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,30 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * find_sqrt - Helper function to find the square root recursively
- * @n: Number to compute square root of
- * @i: Iterator to test square root value
+ * find_sqrt - Finds the natural square root of a number
+ * @n: Number to find the square root of
+ * @guess: Guess for the square root
  *
- * Return: Square root of n, -1 if no natural square root is found
+ * Return: Square root of n, or -1 if no natural square root
  */
-int find_sqrt(int n, int i)
+int find_sqrt(int n, int guess)
 {
-	if (i * i > n)
-		return (-1);
-
-	if (i * i == n)
-		return (i);
-
-	return (find_sqrt(n, i + 1));
+    if (guess * guess > n)
+        return (-1);
+    if (guess * guess == n)
+        return (guess);
+    return (find_sqrt(n, guess + 1));
 }
 
 /**
- * _sqrt_recursion - Computes the natural square root of a number
- * @n: Number to compute square root of
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: Number to find the square root of
  *
- * Return: Natural square root of n, -1 if n does not have a natural square root
- *
- * Description: Uses recursion to find the square root.
+ * Return: Square root of n, or -1 if no natural square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-
-	return (find_sqrt(n, 0));
+    if (n < 0)
+        return (-1);
+    return (find_sqrt(n, 0));
 }
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Function prototypes */
+int _sqrt_recursion(int n);
+
+#endif /* HOLBERTON_H */
