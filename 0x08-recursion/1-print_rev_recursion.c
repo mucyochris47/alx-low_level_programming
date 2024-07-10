5ad361608196dd797f1ade8bf8e4566cdcbcac08
mucyochris47 <mucyochris250@gmail.com>
2024-07-10 11:04:41 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,9 +9,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
+	if (*s == '\0')
+		return;
 
-    _print_rev_recursion(s + 1); /* Recursively move to end of string */
-    _putchar(*s); /* Print current character after recursion */
+	_print_rev_recursion(s + 1); /* Recursively move to end of string */
+	_putchar(*s); /* Print current character after recursion */
 }
