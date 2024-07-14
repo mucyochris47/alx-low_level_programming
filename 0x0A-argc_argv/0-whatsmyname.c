fae5aefb76d108a99315cadc23232f114cd0024b
mucyochris47 <mucyochris250@gmail.com>
2024-07-14 00:42:28 -0700

    Completed Task 0 - Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    /* unused parameter argc */
+    (void)argc;
+    printf("%s\n", argv[0]);
+    return 0;
+}
