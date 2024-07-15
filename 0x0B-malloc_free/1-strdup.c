7cf8320ddf874be21b913255144d7d9337bfd0d3
mucyochris47 <mucyochris250@gmail.com>
2024-07-15 01:43:26 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,23 +10,23 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    unsigned int i, len;
+	char *duplicate;
+	unsigned int i, len;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    len = 0;
-    while (str[len])
-        len++;
+	len = 0;
+	while (str[len])
+		len++;
 
-    duplicate = malloc(sizeof(char) * (len + 1));
+	duplicate = malloc(sizeof(char) * (len + 1));
 
-    if (duplicate == NULL)
-        return (NULL);
+	if (duplicate == NULL)
+		return (NULL);
 
-    for (i = 0; i <= len; i++)
-        duplicate[i] = str[i];
+	for (i = 0; i <= len; i++)
+		duplicate[i] = str[i];
 
-    return (duplicate);
+	return (duplicate);
 }
