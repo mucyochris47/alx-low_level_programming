b6527602f4066c8cbebb72b3be29f62519f1aafa
mucyochris47 <mucyochris250@gmail.com>
2024-07-22 13:03:16 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,12 @@
 #define DOG_H
 
 /**
- * struct dog - structure for dog information
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - A dog's basic info
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This struct defines the basic information of a dog.
  */
 struct dog
 {
@@ -13,6 +15,15 @@ struct dog
     float age;
     char *owner;
 };
+
+/* Typedef for struct dog */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
