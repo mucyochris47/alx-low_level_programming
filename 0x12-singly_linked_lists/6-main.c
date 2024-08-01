8d87f66a700c0cb218e1bbd88cd29180f52e7f14
mucyochris47 <mucyochris250@gmail.com>
2024-08-01 11:09:48 -0700

    task 6

---
+++ 0x12-singly_linked_lists/6-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the C program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("Testing assembly program...\n");
+    return (0);
+}
+
