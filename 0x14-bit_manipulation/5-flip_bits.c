c0992583acfa710b6abb71877ab9c45f73f7fa15
mucyochris47 <mucyochris250@gmail.com>
2024-08-08 10:44:36 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,13 +11,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor = n ^ m;  // XOR to find differing bits
+    unsigned long int xor = n ^ m;  /* XOR to find differing bits */
     unsigned int count = 0;
 
     while (xor)
     {
-        count += xor & 1;  // Increment count if the last bit is 1
-        xor >>= 1;         // Shift right to check the next bit
+        count += xor & 1;  /* Increment count if the last bit is 1 */
+        xor >>= 1;         /* Shift right to check the next bit */
     }
 
     return count;
