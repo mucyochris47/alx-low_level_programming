a7f63dce8df0426742acb16b006ca0523c6a2d9d
mucyochris47 <mucyochris250@gmail.com>
2025-01-28 02:17:42 -0800

    task 10

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,1 +1,16 @@
+#include "sort.h"
+
+/**
+ * exponential_sort - Sorts an array of integers in ascending order
+ *                    using the Exponential search and a sorting algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void exponential_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    /* Your code logic here */
+}
 
